51010b40238144431bdb4ec72f488a77bf33b40d
pre-alpha-final <jacek.mowinski@gmail.com>
2023-05-13 19:04:29 +0200

    The project

---
+++ src/Animation/Animation.cs
@@ -0,0 +1,34 @@
+﻿namespace Animation;
+
+internal class Animation
+{
+    public async Task Run(int fps, int duration, bool renderWait, Func<int, double, Task> taskFunc)
+    {
+        var startTime = DateTime.Now;
+        var currentTimestamp = 0;
+        do
+        {
+            await RenderFrame(fps, duration, renderWait, taskFunc, currentTimestamp);
+            currentTimestamp = (int)(DateTime.Now - startTime).TotalMilliseconds;
+        }
+        while (currentTimestamp < duration);
+
+        currentTimestamp = duration;
+        await RenderFrame(fps, duration, renderWait, taskFunc, currentTimestamp);
+    }
+
+    private async Task RenderFrame(int fps, int duration, bool renderWait, Func<int, double, Task> taskFunc, int currentTimestamp)
+    {
+        if (renderWait)
+        {
+            var worktask = Task.Run(() => taskFunc(currentTimestamp, (double)currentTimestamp / duration));
+            var delayTask = Task.Delay(1000 / fps);
+            await Task.WhenAll(worktask, delayTask);
+        }
+        else
+        {
+            _ = Task.Run(() => taskFunc(currentTimestamp, (double)currentTimestamp / duration));
+            await Task.Delay(1000 / fps);
+        }
+    }
+}
---
+++ src/Animation/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Animation;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Same animation 1 fps
+        await new Animation().Run(1, 4000, true, async (timestamp, progress) =>
+        {
+            await WriteStuff(progress);
+        });
+        Console.Clear();
+
+        // Same animation 10 fps
+        await new Animation().Run(10, 4000, true, async (timestamp, progress) =>
+        {
+            await WriteStuff(progress);
+        });
+        Console.Clear();
+
+        // Same animation 30 fps
+        await new Animation().Run(30, 4000, true, async (timestamp, progress) =>
+        {
+            await WriteStuff(progress);
+        });
+        Console.Clear();
+
+        // Same animation 60 fps
+        await new Animation().Run(60, 4000, true, async (timestamp, progress) =>
+        {
+            await WriteStuff(progress);
+        });
+        Console.Clear();
+
+        Console.WriteLine("Done");
+        Console.ReadKey();
+    }
+
+    private static Task WriteStuff(double progress)
+    {
+        Console.SetCursorPosition(0, 0);
+        Console.Write($"Progress: {progress}");
+        Console.WriteLine($"          ");
+        for (var i = 0; i < progress * 100; i++)
+        {
+            Console.Write(" ");
+        }
+        Console.Write("*");
+
+        return Task.CompletedTask;
+    }
+}
